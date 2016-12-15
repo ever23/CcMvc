fdda6ff80731ad70531522f91e6b89f8f2fd2571
Enyerber Franco <enyerverfranco@gmail.com>
2016-12-15 07:24:09 -0430

    mejoras

--- docHtml.bat
+++ docHtml.bat
@@ -1,1 +1,1 @@
-"E:\phpdocumentor2\bin\phpdoc.bat" "run" "--ansi" "--progressbar" "--directory" "E:/www/CcMvc/src/" "--target" "E:/www/CcMvc/documentacion/" "--title" "CcMvc"  "--sourcecode" "--cache-folder" "E:/documentorcache/" "--template" "responsive-twig" "--defaultpackagename" "CcMvc"
+"E:\phpdocumentor2\bin\phpdoc.bat" "run" "--ansi" "--progressbar" "--directory" "E:/www/CcMvc/src/" "--target" "E:/www/CcMvc/documentacion/" "--title" "CcMvc"  "--sourcecode" "--cache-folder" "E:/documentorcache/" "--defaultpackagename" "CcMvc"
